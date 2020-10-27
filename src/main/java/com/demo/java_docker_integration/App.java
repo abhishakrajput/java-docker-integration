cf054eaa64bd32dc573bfec1f2d79150b5873397
Abhishek.chauhan <abhishek.chauhan@wheebox.com>
2020-10-27 06:48:04 +0530

    my first coimmit

---
+++ src/main/java/com/demo/java_docker_integration/App.java
@@ -0,0 +1,13 @@
+package com.demo.java_docker_integration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
